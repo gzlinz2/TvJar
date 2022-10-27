a8f03018c496ef65d98da26c50f075515713a26d
gzlinz2 <107169210+gzlinz2@users.noreply.github.com>
2022-10-27 09:01:33 +0800

    Update Init.java
    update

--- app/src/main/java/com/github/catvod/spider/Init.java
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -1,6 +1,3 @@
-//
-// Decompiled by Jadx - 368ms
-//
 package com.github.catvod.spider;
 
 import android.content.Context;
@@ -8,6 +5,6 @@ import com.github.catvod.crawler.SpiderDebug;
 
 public class Init {
     public static void init(Context context) {
-        SpiderDebug.log("俊哥专用，意外得到请勿外泄 谢谢");
+        SpiderDebug.log("嘿嘿嘿");
     }
 }
