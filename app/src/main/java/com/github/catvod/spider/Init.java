112b5e61ee32e8f398db7f9fcf8791ea3640e7ac
gzlinz2 <107169210+gzlinz2@users.noreply.github.com>
2022-10-27 08:53:31 +0800

    Add files via upload

---
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -0,0 +1,13 @@
+//
+// Decompiled by Jadx - 368ms
+//
+package com.github.catvod.spider;
+
+import android.content.Context;
+import com.github.catvod.crawler.SpiderDebug;
+
+public class Init {
+    public static void init(Context context) {
+        SpiderDebug.log("俊哥专用，意外得到请勿外泄 谢谢");
+    }
+}
