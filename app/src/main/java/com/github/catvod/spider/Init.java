5b9f142de700fe5cd84183086003d173da8c31f8
nikolin601 <76394646+nikolin601@users.noreply.github.com>
2022-10-29 01:31:43 +0800

    Add files via upload

---
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -0,0 +1,10 @@
+package com.github.catvod.spider;
+
+import android.content.Context;
+import com.github.catvod.crawler.SpiderDebug;
+
+public class Init {
+    public static void init(Context context) {
+        SpiderDebug.log("太虚为鼎，太极为炉，清静为基。");
+    }
+}
